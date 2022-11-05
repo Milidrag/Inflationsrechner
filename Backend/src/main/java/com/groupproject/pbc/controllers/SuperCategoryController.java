ec94345c8a611d7aa35b4fe143972ea9cb62c284
Ilona Siarheichyk <fhb180705@fh-vie.ac.at>
2022-11-05 20:16:21 +0100

    Fix von files

--- Backend/src/main/java/com/groupproject/pbc/controllers/SuperCategoryController.java
+++ Backend/src/main/java/com/groupproject/pbc/controllers/SuperCategoryController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@CrossOrigin(origins = "*")
 @RequestMapping(path="/pbc")
 public class SuperCategoryController {
 
--- Backend/src/main/java/com/groupproject/pbc/repositories/ProductRepository.java
+++ Backend/src/main/java/com/groupproject/pbc/repositories/ProductRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface ProductController extends CrudRepository<Product, Integer> {
+public interface ProductRepository extends CrudRepository<Product, Integer> {
 
     List<Product> findByProdName(String name);
 
