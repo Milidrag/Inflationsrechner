73358ca77e160c96e5302f5f760785e9b109a696
Ilona Siarheichyk <fhb180705@fh-vie.ac.at>
2022-11-05 19:42:23 +0100

    frontend initial config

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import axios from "axios";
 
 function App() {
-  return (
+    const [product, setProduct] = useState(null);
+    function createPost() {
+        axios
+            .post('//localhost:8080/pbc/super-category?name=Kleidung')
+            .then((response) => {
+                setProduct(response.data);
+            });
+    }
+    if (!product) return (
+        <>
+            <button onClick={createPost} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                Button
+            </button>
+            <p>No product!</p>
+        </>)
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+        <button onClick={createPost} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            Button
+        </button>
     </div>
   );
 }
